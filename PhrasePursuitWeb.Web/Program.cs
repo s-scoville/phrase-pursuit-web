89fc1840ec9a35b6875be3ab307c8d4e5004c4b7
Steven Scoville <s.scoville.dev@outlook.com>
2026-08-22 11:01:52 -0500

    feat: configure web application services

--- PhrasePursuitWeb.Web/Program.cs
+++ PhrasePursuitWeb.Web/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PhrasePursuitWeb.Web;
+using PhrasePursuitWeb.Core.Interfaces;
+using PhrasePursuitWeb.Core.Managers;
+using PhrasePursuitWeb.Web.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(
+    sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStorageService, BrowserStorageService>();
+builder.Services.AddScoped<StatisticsManager>();
+builder.Services.AddScoped<SpinManager>();
 
 await builder.Build().RunAsync();
